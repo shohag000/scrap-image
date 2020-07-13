4c237dd8ee677e9daaa5fe7685c27a5b177a75d9
shohag000 <shohagbiswas000@gmail.com>
2020-07-13 18:43:09 +0600

    taken input from user

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ import re
 import requests, urllib
 from bs4 import BeautifulSoup
 
-site = 'https://unsplash.com/t/wallpapers'
+site = input("Enter your url: ") 
+try:
+    response = requests.get(site)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    img_tags = soup.find_all('img')
 
-response = requests.get(site)
+    urls = [img['src'] for img in img_tags]
 
-soup = BeautifulSoup(response.text, 'html.parser')
-img_tags = soup.find_all('img')
-
-urls = [img['src'] for img in img_tags]
-
-x=0
-image = None
-for url in urls:
-    if url[:1]=="/":
-        pass
-    else:
-        image = url
-        print(image)
-    x= x+1
-    output = open("images/{}.jpg".format(x),"wb")
-    output.write(urllib.request.urlopen(image).read())
-    output.close()
+    x=0
+    image = None
+    for url in urls:
+        if url[:1]=="/":
+            pass
+        else:
+            image = url
+        x= x+1
+        output = open("images/{}.jpg".format(x),"wb")
+        output.write(urllib.request.urlopen(image).read())
+        output.close()
+except:
+    print("Invalid url")
